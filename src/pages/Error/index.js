4e125692964042e88c38093a53c19698d7d21cf6
Dianciré Diallo <diaancir@gmail.com>
2022-11-16 09:46:50 +0000

    Page erreur + Responsive

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+import '../../styles/Error.css'
+
 function Error() {
     return (
-        <div>
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
+        <div className='error_content'>
+            <div className='error_404'>404</div>
+            <div className='error_texte'>Oups! La page que vous demandez n'existe pas.</div>
+            <Link to="/" className='error_link'>Retourner sur la page d’accueil</Link>
         </div>
     )
 }
