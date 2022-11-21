2d3f313de01dde2cc32523cbd3d15e5e4b8b3eec
Dianciré Diallo <diaancir@gmail.com>
2022-11-21 21:04:35 +0000

    Finalisation de la page logement + Responsive

--- src/pages/Lodging/index.js
+++ src/pages/Lodging/index.js
@@ -1,16 +1,28 @@
 import { useParams } from 'react-router-dom'
 import Carrousel from '../../components/Carrousel'
+import Collapse from '../../components/Collapse'
+import LodgingInfo from '../../components/LodgingInfo'
 import Lodgings  from '../../logement.json'
+import '../../styles/Lodging.css'
 
 function Lodging() {
 
    const { fichelogement } = useParams();
    const lodgingInfo = Lodgings.find(lodging => lodging.id === fichelogement);
 
-    return (
-      <div className="lodging">
-         <div>
-               <Carrousel slidesImg={lodgingInfo.pictures}/>
+   return (
+      <div>
+         <Carrousel slidesImg={lodgingInfo.pictures}/>
+         <LodgingInfo description={lodgingInfo}/>
+         <div className='lodging_collapse'>
+            <div className='lodging_collapse_element'>
+               <Collapse title={"Description"} texte={lodgingInfo.description}/>
+            </div>
+            <div className='lodging_collapse_element'>
+               <Collapse title={"Equipements"} texte={lodgingInfo.equipments.map((Equipement_txt) => (
+                  <li className='lodging_equipement' key={Equipement_txt}>{Equipement_txt}</li>
+               ))} />
+            </div>
          </div>
       </div>
     );
